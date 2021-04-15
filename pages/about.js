a21b878393a21d49f4f8f0111871088c8bcfa6f0
robgmerrill <robgmerrill>
2021-04-15 12:36:58 -0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+          commodi repellendus rerum pariatur omnis numquam odio iure eaque hic
+          iste.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          saepe nostrum tempore nobis omnis inventore placeat accusamus dolor
+          maxime quos?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
